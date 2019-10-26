b46f161f90356beb75fdc698e7af153a2164db00
李远宸 <li1048672037@gmail.com>
2019-10-26 13:05:55 +0800

    Upload To Github

---
+++ DFAHacker.cpp
@@ -0,0 +1,97 @@
+﻿#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <tchar.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <fstream>
+#pragma warning(disable:4996)
+using namespace std;
+void Wchar_tToString(std::string& szDst, wchar_t* wchar)
+{
+	wchar_t* wText = wchar;
+	DWORD dwNum = WideCharToMultiByte(CP_OEMCP, NULL, wText, -1, NULL, 0, NULL, FALSE);
+	char* psText;
+	psText = new char[dwNum];
+	WideCharToMultiByte(CP_OEMCP, NULL, wText, -1, psText, dwNum, NULL, FALSE);
+	szDst = psText;
+	delete[]psText;
+}
+
+int CheckReplace(string filename, string ReplaceContent, string ChangeContent) {
+	ifstream file;
+	int n = 0;//统计行数的变量
+	string tmp;
+	file.open(filename, ios::in);
+	if (file.fail()) {
+		return 0;
+	}
+	else {
+		string TMP;//存放读取行内容的临时变量
+		while (!file.eof()) {
+			file >> TMP;
+			if (TMP != ReplaceContent) {
+				n++;
+			}
+			else {
+				n++;
+				break;
+			}
+		}
+		file.close();
+		ifstream fp;
+		fp.open(filename);
+		string strFileData = "";
+		int line = 1;
+		char tmpLineData[1024] = { 0 };
+		while (fp.getline(tmpLineData, sizeof(tmpLineData))) {
+			if (line == n) {
+				system("pause");
+				strFileData += ChangeContent;
+				strFileData += "\n";
+			}
+			else {
+				strFileData += tmpLineData;
+				strFileData += "\n";
+			}
+			line++;
+		}
+		fp.close();
+		//写入文件
+		ofstream out;
+		out.open(filename);
+		out.flush();
+		cout << strFileData << endl;
+		out << strFileData;
+		out.close();
+	}
+}
+
+int main()
+{
+	std::cout << "按下任意键检查DFA程序配置是否存在！\n";
+	system("pause");
+	wchar_t buffer[MAX_PATH];
+	DWORD code = GetEnvironmentVariable(L"APPDATA", buffer, MAX_PATH);
+	string pathTMP = "\\DFAssist\\config.ini";
+	string Buffer1;
+	Wchar_tToString(Buffer1, buffer);
+	string path = Buffer1 + pathTMP;
+	ifstream _file(path, ios::in);
+	if (!_file)
+	{
+		cout << path << " DFA配置文件读取失败，请确保您已经运行了一次DFA并正确彻底地关闭。" << endl;
+	}
+	else
+	{
+		cout << path << " DFA数据确认完成，即将执行修改。" << endl;
+		system("pause");
+		string filename = path;
+		string a = "crack=0";
+		string b = "crack=1";
+		CheckReplace(filename, a, b);
+		system("pause");
+		return 0;
+	}
+}
